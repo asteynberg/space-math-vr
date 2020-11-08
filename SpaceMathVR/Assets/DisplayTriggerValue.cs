3289813d45a3c71d00c5a22e043f3dd55b59f315
Jadiker <happylondon4@gmail.com>
2020-11-07 23:50:53 -1000

    init commit - can see hands

---
+++ SpaceMathVR/Assets/DisplayTriggerValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayTriggerValue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float leftHandTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+        gameObject.GetComponent<TextMesh>().text = leftHandTrigger.ToString();
+    }
+}
