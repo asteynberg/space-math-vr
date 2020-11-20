62a812c5d1404b8eb0185ca40aed2b5e70a56045
Jadiker <happylondon4@gmail.com>
2020-11-19 23:28:45 -1000

    demoable

---
+++ SpaceMathVR/Assets/Scripts/DialReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialReader : MonoBehaviour
+{
+    public GameObject dial;
+    // 0 to 360
+    public float initialAngle = 55;
+    public float minimumValue = 0;
+    public float maxiumumValue = 50;
+    public bool round;
+    public float value;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float maxSpread = maxiumumValue - minimumValue;
+        float angle = dial.transform.eulerAngles.y;
+        if (angle < 0)
+        {
+            angle = 360 - angle;
+        }
+        // make sure it's positive and in the range [0, 365)
+        angle = (((angle - initialAngle) % 365) + 365) % 365;
+        value = angle * (maxSpread / 365) + minimumValue;
+        if (round)
+        {
+            value = Mathf.Round(value);
+        }
+        gameObject.GetComponent<TextMesh>().text = value.ToString();
+    }
+}
---
+++ SpaceMathVR/Assets/Scripts/GeneratePoleOnClick.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GeneratePoleOnClick : MonoBehaviour
+{
+    public const string makePoleButtonClickEventName = "pole_button_click";
+    private UnityAction clickListener;
+    public DialReader poleDialReader;
+    public Material poleColor;
+    public Transform poleSpawn;
+    [HideInInspector]
+    public GameObject pole = null;
+
+    void Awake()
+    {
+        clickListener = new UnityAction(createPole);
+    }
+    void OnEnable()
+    {
+        EventManager.StartListening(EventManager.EventName.makePoleButtonClicked, clickListener);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening(EventManager.EventName.makePoleButtonClicked, clickListener);
+    }
+
+    void createPole()
+    {
+        if (pole)
+        {
+            // get rid of the old pole
+            Destroy(pole);
+        }
+        // create a new pole
+        float poleLength = poleDialReader.value * 0.1f - 0.05f;
+        pole = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        pole.GetComponent<MeshRenderer>().material = poleColor;
+        pole.transform.SetPositionAndRotation(poleSpawn.position, poleSpawn.rotation);
+        pole.transform.localScale = new Vector3(0.05f, poleLength, 0.05f);
+        Interactable interactable = pole.AddComponent<Interactable>() as Interactable;
+        interactable.interactionType = Interactable.InteractionType.grab;
+        Rigidbody rigidbody = pole.AddComponent<Rigidbody>() as Rigidbody;
+        rigidbody.isKinematic = true;
+        rigidbody.useGravity = false;
+        CapsuleCollider collider = pole.GetComponent<CapsuleCollider>();
+        collider.isTrigger = true;
+        Debug.Log("POLE CREATED!");
+    }
+}
